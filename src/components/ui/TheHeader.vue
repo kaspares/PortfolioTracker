2c34f86fbe0557ef3be9417db47b93c137c81d0b
Kacper Kazimierczuk <kacper240804@gmail.com>
2026-03-17 13:54:57 +0100

    feat: add header

---
+++ src/components/ui/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="navbar bg-primary shadow-sm">
+        <div class="navbar-center w-full justify-center">
+            <p class="text-xl text-primary-content">Portfolio Tracker</p>
+        </div>
+    </div>
+</template>
